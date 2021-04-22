fda202bd913e2c2205c9d4e474eb0b2173e1626b
tam4477 <48490702+tam4477@users.noreply.github.com>
2021-04-22 19:50:53 -0400

    Update fitness.c

--- fitness.c
+++ fitness.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "fitness.h"
 
-static exercise *db[MAX_ITEMS];
+static exercise *db[MAX_EXERCISES];
 static int num_items = 0;
 static exercise *complt[MAX_EXER_COMPLT];
 static int exercises_complt = 0;
@@ -14,11 +14,11 @@ int read_db(char *filename) {
                 return -1;
 
         char local_item[100], cat[100];
-        int itemnum, laps, sets, reps, calorieburn, resistance;
-        double mile_distance, mins;
-        for (num_items = 0; fscanf(fp, "%d %s %s %c %d %lf %d ", &itemnum, cat, local_item, &mins, &laps, &mile_distance, &sets, &reps, &resistance, &calorieburn) != EOF; num_items++) {
+        int exercisenum, mins, laps, sets, reps, calorieburn, resistance;
+        double mile_distance;
+        for (num_items = 0; fscanf(fp, "%d %s %s %d %d %lf %d %d %d %d ", &exercisenum, cat, local_item, &mins, &laps, &mile_distance, &sets, &reps, &resistance, &calorieburn) != EOF; num_items++) {
                 db[num_items] = malloc(sizeof(exercise));
-                db[num_items]->itemnum = itemnum;
+                db[num_items]->exercisenum = exercisenum;
                 strcpy(db[num_items]->name, local_item);
                 db[num_items]->category = str_to_category(cat);
                 db[num_items]->mins = mins;
@@ -38,7 +38,7 @@ int read_db(char *filename) {
         if ((fp = fopen(filename, "w")) == NULL)
                 return -1;
         for (int i = 0; i < num_items; i++) {
-                fprintf(fp, "%d %s %s %c %d %.2lf %d\n", db[i]->itemnum, category_to_str(db[i]->category), db[i]->name, db[i]->size, db[i]->quantity, db[i]->cost, db[i]->onsale);
+                fprintf(fp, "%d %s %s %c %d %.2lf %d\n", db[i]->exercisenum, category_to_str(db[i]->category), db[i]->name, db[i]->mins, db[i]->laps, db[i]->mile_distance, db[i]->sets, db[i]->reps, db[i]->resistance, db[i]->calorieburn);
         }
         fclose(fp);
         return 0;
