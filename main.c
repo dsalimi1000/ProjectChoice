361d34f2ebd93994356a5b113e33d772ab0704fb
dsalimi1000 <67076009+dsalimi1000@users.noreply.github.com>
2021-04-24 16:20:14 -0400

    Update main.c

--- main.c
+++ main.c
@@ -82,6 +82,27 @@ int get_menu_selection(int num) {
 				printf("New user %s was added.\n", user_name);
 			}
 			break;
+			case 3:
+			display_exercises();
+		case 3:
+			display_exercises();
+			break;
+		case 4:
+			printf("Enter the user number to switch to(1-5):  ");
+			scanf("%d", &usernum);
+			swap_user(usernum-1);
+			break;
+		case 5: 
+			printf("\n Metabolic Rate:  ");
+			printf("%lf", metabolic_wt());
+			printf("\n Lose a pound a week:  ");
+			printf("%lf", lose_wt());
+			printf("\n Gain a pound a week:  ");
+			printf("%lf", gain_wt());
+			printf("\n Body fat percentage:  ");
+			printf("%lf", body_fat());
+			printf("\n");
+			break;	
 		case 6:
 			printf("Would you like to update your age? Y or N. ");
 			scanf("%s", ans);
@@ -104,24 +125,6 @@ int get_menu_selection(int num) {
 				scanf("%lf", &height);
 				update_user_height(height);
 			}
-		case 3:
-			display_exercises();
-		case 4:
-			printf("Enter the user number to switch to(1-5):  ");
-			scanf("%d", &usernum);
-			swap_user(usernum-1);
-			break;
-		case 5: 
-			printf("\n Metabolic Rate:  ");
-			printf("%lf", metabolic_wt());
-			printf("\n Lose a pound a week:  ");
-			printf("%lf", lose_wt());
-			printf("\n Gain a pound a week:  ");
-			printf("%lf", gain_wt());
-			printf("\n Body fat percentage:  ");
-			printf("%lf", body_fat());
-			printf("\n");
-			break;
 		case 7:
 			//write_users(filename);
 			printf("Your profile was saved.");
